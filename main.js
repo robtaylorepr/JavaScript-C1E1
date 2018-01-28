161cda54991a4becd82ae1f902f27395b26b10a8
robtaylorepr <epr1935@icloud.com>
2018-01-28 12:09:51 -0500

    Learning JavaScript C1E1

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).ready(function()
+{'use strict';
+  paper.install(window);
+  paper.setup(document.getElementById('mainCanvas'));
+
+  var c = Shape.Circle(200, 200, 80); c.fillColor = 'black';
+  var text = new PointText(200, 200); text.justification = 'center'; text.fillColor = 'white'; text.fontSize = 20;
+  text.content = 'hello world';
+  paper.view.draw();
+
+  var tool = new Tool();
+  tool.onMouseDown = function(event) {
+    var c = Shape.Circle(event.point.x, event.point.y, 20); c.fillColor = 'green';
+    c.fillColor = 'green';
+    paper.view.draw();
+    };
+
+  console.log('main.js loaded');
+});
